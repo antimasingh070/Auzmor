24ba45b1ccb8d13379936033dd3d24000fba7fa8
Antima Singh <66549508+antimasingh070@users.noreply.github.com>
2022-07-12 10:12:36 +0530

    Create Remove Linked List Elements.rb

---
+++ Linkedlist/Remove Linked List Elements.rb
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @param {Integer} val
+# @return {ListNode}
+def remove_elements(head, val)
+  head = head.next 
+while !head.nil? && head.val == val
+  cur = head
+  while !cur.nil? && !cur.next.nil?
+    if cur.next.val == val
+      cur.next = cur.next.next
+    else
+      cur = cur.next
+    end
+  end
+  head
+end
