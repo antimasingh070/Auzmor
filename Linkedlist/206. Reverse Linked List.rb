32a3475abdffb9e8fcc22acf7f6ba43653de12d5
Antima Singh <66549508+antimasingh070@users.noreply.github.com>
2022-07-12 10:28:33 +0530

    Create 206. Reverse Linked List.rb

---
+++ Linkedlist/206. Reverse Linked List.rb
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+  return head if head.nil? || head.next.nil?
+  p = reverse_list(head.next)
+  head.next.next = head
+  head.next = nil
+  p
+end
